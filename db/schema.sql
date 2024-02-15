7e3073bd8929be08108d2cd62f98474e0e34a0a4
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-15 12:15:14 -0500

    added role table

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,16 @@ USE staff_db;
 
 -- create table for department
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
+);
+
+-- create table for role
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
