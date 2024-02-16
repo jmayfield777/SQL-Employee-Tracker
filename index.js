a084b8d9eb738ce1b2f83f9d0cb3fe2d27ff79aa
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-16 18:07:55 -0500

    added switch statement to run function for different choices made on inquirer prompt

--- index.js
+++ index.js
@@ -12,3 +12,63 @@ sequelize.connect(function (err) {
   promptOne();
   console.log('You are now connected to the database');
 });
+
+
+// inquirer promptOne function
+function promptOne() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'userChoice',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'Add Employee',
+        'Update Employee Role',
+        'Remove Employee',
+        'View All Roles',
+        'Add Role',
+        'View All Departments',
+        'View Employees By Department',
+        'Add Department',
+        'Quit'
+      ]
+    }
+  ]).then((res) => {
+    console.log(res.userChoice);
+    switch (res.userChoice) {
+      case 'View All Employees':
+        viewAllEmployees();
+        break;
+      case 'Add Employee':
+        addEmployee();
+        break;
+      case 'Update Employee Role':
+        updateEmployeeRole();
+        break;
+      case 'Remove Employee':
+        removeEmployee();
+        break;
+      case 'View All Roles':
+        viewAllRoles();
+        break;
+      case 'Add Role':
+        addRole();
+        break;
+      case 'View All Departments':
+        viewAllDepartments();
+        break;
+      case 'View Employees By Department':
+        viewAllEmployeesByDepartment();
+        break;
+      case 'Add Department':
+        addDepartment();
+        break;
+      case 'Quit':
+        sequelize.end();
+        break;
+    }
+  }).catch((err) => {
+    if (err) throw err;
+  });
+}
