e85a7fab1b256ae0d08a48cf03fbf9a2ad68bba4
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-16 13:07:54 -0500

    inserted values into employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,4 +17,20 @@ VALUES ('Senior Software Engineer', 120000, 1),
        ('Product Manager', 110000, 5),
        ('Customer Service Representative', 80000, 3),
        ('IT Helpdesk Technician', 75000, 4),
-       ('Sales Associate', 80000, 5);
+       ('Sales Associate', 80000, 5),
+       ('Sales Manager', 90000, 5);
+
+
+-- insert values into employee table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', "Smith", 3, 5),
+       ('Jill', 'Saunders', 1, null),
+       ('Tim', 'Robinson', 6, 5),
+       ('Stephanie', 'Anderson', 5, 7),
+       ('Tony', 'Todd', 10, 11),
+       ('Tiffany', 'Johnson', 9, null),
+       ('Michael', 'Andrews', 8, null),
+       ('Michelle', 'Jones', 2, 1),
+       ('Bruce', 'Peters', 4, 1),
+       ('Barbara', 'Davis', 7, null),
+       ('Francisco', 'Mendez', 11, null);
