b6608ae6398b04efd6db9ae4e73dbc70c4863143
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-16 17:51:18 -0500

    connected to staff_db using connect method

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const inquirer = require('inquirer');
 const sequelize = require('./config/connection.js');
 require('console.table');
 
+// connect to staff_db
+sequelize.connect(function (err) {
+  if (err) throw err;
+
+  // if no error run inquirer promptOne
+  promptOne();
+  console.log('You are now connected to the database');
+});
