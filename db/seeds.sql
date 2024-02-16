673d7b8ae0a41b999f47c12f4dec83db1b4ca362
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-16 12:36:13 -0500

    inserted values into role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,16 @@ VALUES (Engineering),
        (Human Resources),
        (IT),
        (Sales);
+
+-- insert values into role table
+INSERT INTO role (title, salary, department_id)
+VALUES ('Senior Software Engineer', 120000, 1),
+       ('Junior Software Engineer', 100000, 1),
+       ('Social Media Manager', 65000, 2),
+       ('Data Analyst', 95000, 1),
+       ('Marketing Manager', 85000, 2),
+       ('Associate Marketing Manager', 70000, 2),
+       ('Product Manager', 110000, 5),
+       ('Customer Service Representative', 80000, 3),
+       ('IT Helpdesk Technician', 75000, 4),
+       ('Sales Associate', 80000, 5);
